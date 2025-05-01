81e3ab3e3624aebe21f4771ba7d01ca204174c70
NIKHIL NAGELLA <nagellanikhil21@gmail.com>
2025-05-01 12:09:25 +0530

    Add files via upload

--- week 6/string.py
+++ week 6/string.py
@@ -1,1 +1,131 @@
+# my_string='Hello, world'
+# print(my_string)
 
+
+# my_string='Hello, world'
+# print(my_string[0])
+# print(my_string[7])
+
+
+# my_string='Hello, World!'
+# print(my_string[0:5])
+# print(my_string[7:])
+# print(my_string[:5])
+# print(my_string[-6:])
+
+
+
+# string1='Hello'
+# string2='World!'
+# conc_string=(string1+' '+string2)
+# print(conc_string)
+
+
+
+# aizen='Hello world!'
+# length=len(aizen)
+# print(length)
+
+
+
+# aizen='YOKOSO, WATASHI NO SOUL SOCIETY'
+# print(aizen.lower())
+
+
+
+
+# ichigo='bankai, tensa zangetsu'
+# print(ichigo.upper())
+
+
+
+
+# naruto='    dattebayo     '
+# print(naruto.strip())
+
+
+
+# madara='I AM THE GHOST OF THE UCHIHA'
+# print(madara.split())
+
+
+
+
+# pain='hi pain'
+# print(pain.replace('pain','nagato'))
+
+
+
+
+
+# rcb='ee saala cup namde'
+# print(rcb.find('cup'))
+
+
+
+
+# name='itachi'
+# age=50
+# formatted_string = "My name is %s and I am %d years old." % (name, age)
+# print(formatted_string)
+
+
+
+
+# name='aizen'
+# age=1500
+# formattedstring="My name is {} and I am {} years old.".format(name, age)
+# print(formattedstring)
+
+
+
+
+# name='shunsui'
+# age=500
+# message=f"Hello, my name is {name} and i am {age} years old"
+# print(message)
+
+
+
+
+
+# num=500
+# message=f"the number is {num*2}"
+# print(message)
+
+
+
+
+
+# s='aizen is goated'
+# for char in s:
+#   print(char, end=" ")
+
+
+
+
+
+# s='itachi is also goated'
+# i=0
+# while i<len(s):
+#   print(s[i], end=' ')
+#   i+=1
+
+
+
+
+
+# s='obito is god'
+# for i, char in enumerate(s):
+#   print(f"character at index {i} is {char}")
+
+
+
+# s='aizen'
+# print(s[-1::-1])
+
+
+s='aizen'
+char='z'
+index=s.index(char)
+print("the first occurance of", char, "is at index",index)
